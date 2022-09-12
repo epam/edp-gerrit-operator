99828029e06c03fa65ecc42dc06de23aca555e10
Sergiy Kulanov <sergiy_kulanov@epam.com>
2022-09-12 14:15:07 +0300

    [EPMDEDP-10428]: feat: Add webhooks plugin configuration
    Push events to Tekton EventListener
    
    Signed-off-by: Sergiy Kulanov <sergiy_kulanov@epam.com>
    Change-Id: I9c299fcd93413ff15dd1cfdee2adeaca110c3bc3

--- build/configs/scripts/init-all-projects.sh
+++ build/configs/scripts/init-all-projects.sh
@@ -16,6 +16,17 @@ printf \"\$2\\tContinuous Integration Tools\\n\" >> groups
 printf \"\$3\\tProject Bootstrappers\\n\" >> groups
 printf \"\$4\\tDevelopers\\n\" >> groups
 printf \"\$5\\tReadOnly\\n\" >> groups
+
+cat << EOF > webhooks.config
+[remote "changemerged"]
+  url = http://el-gerrit-listener:8080
+  event = change-merged
+
+[remote "patchsetcreated"]
+  url = http://el-gerrit-listener:8080
+  event = patchset-created
+EOF
+
 git add .
 git commit -a -m \"Uploaded EDP Gerrit config\"
 git push origin HEAD:refs/meta/config
